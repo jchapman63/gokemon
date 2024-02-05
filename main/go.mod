27d718210af16146d9a51373be0f199f35dd192f
Jordan Chapman <jordan.chapman@immuta.com>
2024-02-04 21:43:17 -0500

    simple cli package for client

---
+++ main/go.mod
@@ -0,0 +1,3 @@
+module github.com/jchapman63/main
+
+go 1.21.6
---
+++ main/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+
+	"github.com/jchapman63/client"
+)
+
+// / Dev Doc: starting out, I will just ask for simple input to get the server started.  Later, a more refined CLI would be nice.
+func main() {
+	fmt.Printf("Welcome to Gokemon!\n")
+	client.ClientStart()
+}
