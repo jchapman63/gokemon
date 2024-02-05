ebe42139084ec422f7665d65c4fa8e73503d6a9b
Jordan Chapman <jordan.chapman@immuta.com>
2024-02-04 22:19:51 -0500

    simple cli http requests

--- client/cli/cli.go
+++ client/cli/cli.go
@@ -2,10 +2,14 @@ package cli
 
 import (
 	"fmt"
+	"io"
+	"net/http"
 
 	"github.com/jchapman63/server"
 	"github.com/nexidian/gocliselect"
 )
+
+var baseUrl = "http://localhost:8081"
 
 func MainMenu() {
 	menu := gocliselect.NewMenu("How would you like to play?")
@@ -16,8 +20,53 @@ func MainMenu() {
 	choice := menu.Display()
 
 	if choice == "host" {
+		// idealistically, choosing to host builds a docker container that hosts the application for the host player.
+		// this way the CLI is free to select more options that send requests
 		server.Server()
 	} else if choice == "connect" {
-		fmt.Print("Not yet implemented")
+		// connect to the match
+		resp, err := http.Get(baseUrl + "/")
+		if err != nil {
+			fmt.Println("server not found")
+			return
+		}
+		defer resp.Body.Close() // close resp body before function ends
+
+		// read resp body
+		body, err := io.ReadAll(resp.Body)
+		if err != nil {
+			fmt.Println("Error reading response body: ", err)
+			return
+		}
+		fmt.Println("\n", string(body), "\n")
+
+		actionMenu := gocliselect.NewMenu("Attack!")
+		actionMenu.AddItem("Tackle", "tackle")
+		actionMenu.AddItem("Quit", "quit")
+
+		actionChoice := actionMenu.Display()
+
+		if actionChoice == "tackle" {
+			_, err := http.Get(baseUrl + "/damage")
+			if err != nil {
+				fmt.Println("error, disconnecting: ", err)
+				return
+			}
+
+			// see tackle results
+			resp, err := http.Get(baseUrl + "/")
+			if err != nil {
+				fmt.Println("server not found")
+				return
+			}
+			defer resp.Body.Close() // close resp body before function ends
+			// read resp body
+			body, err := io.ReadAll(resp.Body)
+			if err != nil {
+				fmt.Println("Error reading response body: ", err)
+				return
+			}
+			fmt.Println("\n", string(body), "\n")
+		}
 	}
 }
--- main/main.go
+++ main/main.go
@@ -8,6 +8,6 @@ import (
 
 // / Dev Doc: starting out, I will just ask for simple input to get the server started.  Later, a more refined CLI would be nice.
 func main() {
-	fmt.Printf("Welcome to Gokemon!\n")
+	fmt.Printf("Welcome to Gokemon!\n\n")
 	client.ClientStart()
 }
