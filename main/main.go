e205bda0575c4d6923ea83ab5c6f1644e86e8d00
Jordan Chapman <jordan.chapman@immuta.com>
2024-02-21 08:00:59 -0500

    server flags

--- main/main.go
+++ main/main.go
@@ -1,13 +1,24 @@
 package main
 
 import (
+	"flag"
 	"fmt"
+	"slices"
 
 	"github.com/jchapman63/gokemon/client"
+	"github.com/jchapman63/gokemon/server"
 )
 
 // / Dev Doc: starting out, I will just ask for simple input to get the server started.  Later, a more refined CLI would be nice.
 func main() {
 	fmt.Printf("Welcome to Gokemon!\n\n")
-	client.ClientStart()
+
+	flag.Parse()
+	if slices.Contains(flag.Args(), "-s") {
+		fmt.Println("Starting Gokemon Server!")
+		server.Server()
+	} else {
+		fmt.Println("Starting Gokemon Client!")
+		client.ClientStart()
+	}
 }
